3cd36220df67f4732cbfa359babd8a1ff468a21f
Manuel Alférez <manuelalferezruiz@gmail.com>
2023-08-06 18:43:01 +0200

    fix: Removing comments

--- src/app/components/transactions/operators.ts
+++ src/app/components/transactions/operators.ts
@@ -5,10 +5,8 @@ export function removeUnselectedColumns(
   selectedCols: SelectedCol[],
   matrix: TransactionsMatrix
 ) {
-  // Create an array of column indices to keep
   const columnsToKeep = selectedCols.map((col) => col.col);
 
-  // Filter the matrix to keep only the selected columns
   const filteredMatrix = matrix.transactions.map((row) =>
     row.filter((_, colIndex: number) => columnsToKeep.includes(colIndex))
   );
