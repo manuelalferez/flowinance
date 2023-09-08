71d115b8327631bc7c426686523945bc5cd56baf
Manuel Alférez <manuelalferezruiz@gmail.com>
2023-09-08 17:57:51 +0200

    fix: scaping apos

--- src/app/transactions/add/page.tsx
+++ src/app/transactions/add/page.tsx
@@ -7,8 +7,8 @@ export default function Page() {
         <div className="p-8 text-center">
           <h1 className="text-3xl font-bold mb-4">Add a Transaction</h1>
           <p className="text-gray-600">
-            Please fill in the required fields below and click 'Upload' when
-            you're good to go.
+            Please fill in the required fields below and click Upload when you
+            are ready to proceed
           </p>
         </div>
 
