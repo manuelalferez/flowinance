825ab32005aa62583ef28c4fa633c348484d2eeb
Manuel Alférez <manuelalferezruiz@gmail.com>
2023-09-18 15:56:40 +0200

    feat: removed logged as from dashbaord and transactions pages

--- src/app/transactions/transactions.tsx
+++ src/app/transactions/transactions.tsx
@@ -1,12 +1,7 @@
 "use client";
 
 import { AppContext } from "@/lib/context";
-import {
-  decryptTransactions,
-  getTransactions,
-  getUserEmail,
-  getUserId,
-} from "@/lib/utils";
+import { decryptTransactions, getTransactions, getUserId } from "@/lib/utils";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { Button } from "../components/ui/button";
@@ -15,28 +10,20 @@ import { useSupabase } from "../supabase-provider";
 import { Transaction } from "../types/global";
 import { TransactionsTable } from "./ui/transactions-table";
 import { useToast } from "../components/ui/use-toast";
-import { CardDescription } from "../components/ui/card";
 import NoTransactions from "./ui/no-transactions";
-import { LoadingIcon } from "../components/dashboard/ui/loading-icon";
 
 export default function Transactions() {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
   const [loading, setLoading] = useState(true);
-  const [email, setEmail] = useState("");
   const { supabase } = useSupabase();
   const { toast } = useToast();
 
   useEffect(() => {
     const fetchData = async () => {
       const userId = await getUserId(supabase);
-      const userEmail = await getUserEmail(supabase);
-      if (!userEmail) {
-        return;
-      }
       if (!userId) {
         return;
       }
-      setEmail(userEmail);
       const data = await getTransactions(supabase);
 
       if (!data) {
@@ -57,17 +44,9 @@ export default function Transactions() {
   return (
     <div className="min-h-screen mb-2 w-3/4">
       <div>
-        <h1 className="text-4xl font-semibold text-gray-800 mb-2">
+        <h1 className="text-4xl font-semibold text-gray-800 mb-6">
           Transactions
         </h1>
-        <CardDescription className="mb-4">
-          Logged as: {}
-          {loading ? (
-            <LoadingIcon />
-          ) : (
-            <span className="bg-emerald-100 p-1 rounded-sm">{email}</span>
-          )}
-        </CardDescription>
       </div>
       <AppContext.Provider value={{ transactions }}>
         <div className="flex items-start mb-10 gap-2 ">
