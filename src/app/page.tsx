849a0a15c8e87bcb5ba8f2fb3739a5775444e18e
Manuel Alférez <manuelalferezruiz@gmail.com>
2023-09-21 08:29:05 +0200

    fix: fixing svg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -92,26 +92,26 @@ export default async function Home() {
                   >
                     <g fill="none" stroke="currentColor">
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="4.667"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="4.667"
                         d="M19.036 44c-.98-3.195-2.458-5.578-4.435-7.147c-2.965-2.353-7.676-.89-9.416-3.318c-1.74-2.428 1.219-6.892 2.257-9.526c1.039-2.634-3.98-3.565-3.394-4.313c.39-.499 2.927-1.937 7.609-4.316C12.987 7.794 17.9 4 26.398 4C39.144 4 44 14.806 44 21.68c0 6.872-5.88 14.276-14.256 15.873c-.749 1.09.331 3.24 3.24 6.447"
                       />
                       <path
-                        stroke-linejoin="round"
-                        stroke-width="4"
+                        strokeLinejoin="round"
+                        strokeWidth="4"
                         d="M19.5 14.5c-.654 2.534-.46 4.314.583 5.339c1.042 1.024 2.818 1.695 5.328 2.01c-.57 3.269.125 4.802 2.083 4.6c1.958-.201 3.135-1.015 3.53-2.44c3.06.86 4.719.14 4.976-2.16c.385-3.45-1.475-6.201-2.238-6.201c-.762 0-2.738-.093-2.738-1.148s-2.308-1.65-4.391-1.65s-.83-1.405-3.69-.85c-1.907.37-3.055 1.203-3.443 2.5Z"
                         clip-rule="evenodd"
                       />
                       <path
-                        stroke-linecap="round"
-                        stroke-width="4"
+                        strokeLinecap="round"
+                        strokeWidth="4"
                         d="M30.5 25.5c-1.017.631-2.412 1.68-3 2.5c-1.469 2.05-2.66 3.298-2.92 4.608"
                       />
                     </g>
                   </svg>
                   <div className="space-y-2">
-                    <h3 className="font-bold">AI</h3>
+                    <h3 className="font-bold">Artificial Intelligence</h3>
                     <p className="text-sm text-muted-foreground">
                       Automatic transaction categorization and file cleaning.
                     </p>
