a723f6b4f37e40eda8b67dceac5940cc8ac04485
Suvendu-UI <b123141@iiit-bh.ac.in>
2024-10-15 14:04:21 +0530

    corrected the build error and changed class to className

--- src/app/signin/SeePassword.tsx
+++ src/app/signin/SeePassword.tsx
@@ -1,9 +1,15 @@
 export const seePassword = () => {
-    const ele = document.querySelector('input[type="password"]');
-    if (ele) {
-      ele.type = "text";
-    } else {
-      const ele = document.querySelector('input[type="text"]');
-      if (ele) ele.type = "password";
-    }
-}
+  const ele = document.querySelector('input[type="password"]') as HTMLInputElement;
+  if (ele) 
+  {
+    ele.type = "text";
+  } 
+  else
+  {
+    const ele = document.querySelector('input[type="text"]') as HTMLInputElement;
+    if (ele) 
+      {
+      ele.type = "password";
+      }
+  }
+};
--- src/app/signin/auth-ui.tsx
+++ src/app/signin/auth-ui.tsx
@@ -40,7 +40,7 @@ export default function AuthUI() {
         theme="light"
       />
       <div className="text-sm flex flex-col text-muted-foreground">
-        <button type="button" onClick={() => seePassword()} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">See password</button>
+        <button type="button" onClick={() => seePassword()} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">See password</button>
         Would you like to give it a try? Feel free to use our demo account 😉
         <p>
           email: <code className="text-sm ">tiffs_purview0r@icloud.com</code>
